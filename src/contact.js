a7e652892d9a056975c855f75fc496c130df9de5
Michael <82574480+ptolemymicheal@users.noreply.github.com>
2022-11-05 23:32:58 +0100

    Reset

--- src/contact.js
+++ src/contact.js
@@ -4,39 +4,43 @@ import './App.css';
 
 function Contact() {
     return (
-        <div className="Contact">
-            <form classname="contact">
-                <fieldset>
-                    <h1>Send a message</h1>
-                    <p>Hi there, contact me to ask anything you have in mind</p>
+        <div className="contact_form">
+            <header>
 
-                    <div classname="Wrap">
-                        <label for="first_name" id="first_name">Your first name</label>
-                        <input type="text" id="name" required placeholder="Enter your First Name" />
-                    </div>
+                <h1>Contact Me</h1>
+                <p>Hi there, contact me to ask anything you have in mind</p>
+            </header>
+            <form action=''>
+                < div classname='contact_flex'>
 
-                    <div classname="Wrap">
-                        <label for="last_name" id="last_name">Your last name</label>
-                        <input type="text" id="last_name" required placeholder="Enter your Last Name" />
-                    </div>
+                    <label for="first_name" id="first_name" className='input'>Your first name</label>
+                    <input type="text" id="name" required placeholder="Enter your First Name" />
 
 
-                    <div classname="Wrap">
-                        <label for="email" id="email-label">Your email</label>
-                        <input type="email" id="email" required placeholder="Your@email.com" />
-                    </div>
+
+                    <label for="last_name" id="last_name" className='input'>Your last name</label>
+                    <input type="text" id="last_name" required placeholder="Enter your Last Name" />
+
+                </div>
+
+                <div classname="Wrap">
+                    <label for="email" id="email-label">Your email</label>
+                    <input type="email" id="email" required placeholder="Your@email.com" className='input' />
+                </div>
 
 
-                    <div classname="Wrap">
-                        <label for="message" id="messsage-label"></label>
-                        <textarea id="message" rows="5" cols="20" name="message" placeholder="Send me a message and i'll reply you as soon as possible" required></textarea>
-                    </div>
+                <div classname="Wrap">
+                    <label for="message" id="messsage-label"></label>
+                    <textarea id="message" cols="30" rows="10" name="message" placeholder="Send me a message and i'll reply you as soon as possible..." required></textarea>
+                </div>
+                <div className='check'>
+                    <input type="checkbox" required /> <span>You agree to providing your data to Zuri who may contact you.</span>
+                </div>
 
+                <div classname="sub">
+                    <button type="submit" id='btn_submit'>Send Message</button>
+                </div>
 
-                    <div classname="sub">
-                        <button type="submit" id='btn_submit'>Send Message</button>
-                    </div>
-                </fieldset>
 
             </form>
 
